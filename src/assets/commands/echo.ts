8998c4ec0ca19d595df44350cd1c66321f3e56b3
senvildev <realsenvil@gmail.com>
2024-07-29 21:10:10 +0200

    found another issue and fixed it like the one before

--- src/assets/commands/echo.ts
+++ src/assets/commands/echo.ts
@@ -6,7 +6,7 @@ export default {
 	execute(args : object[]) {
 		// connect all arguments into one returned output
 		let output : string = "";
-		for (const option of options)
+		for (const arg of args)
 			output += ` ${option}`;
 		// creates a message with the output
 		CREATE_MESSAGE(output);
